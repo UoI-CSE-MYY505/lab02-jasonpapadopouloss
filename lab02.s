511dd6173090f7fc76c38726352447ff8084f2f0
jasonpapadopouloss <147520672+jasonpapadopouloss@users.noreply.github.com>
2024-10-20 20:15:00 +0300

    Update lab02.s

--- lab02.s
+++ lab02.s
@@ -13,6 +13,19 @@ prog:
 # Write your code here!
 # Do not remove the prog label or write code above it!
 #-----------------------------
+findLast:
+    add t2 , zero , zero # to index
+    add s0 , zero , zero # diefthnisi epistrofis
+loop:
+    beq a1, t2 , done
+    slli t1 , t2 , 2
+    add t1 , t1 , a0
+    lw t3 , 0(t1)
+    bne t3, a2, next
+    add s0 , t1 , zero
+next:
+    addi t2, t2 , 1
+    j    loop
 done:
     addi a7, zero, 10 
     ecall
